a86e9b3392b0a1fca251188af1497b0799c1441b
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-03-16 13:47:54 +1100

    hook into remove button

--- src/scripts/snippets_CartItem.js
+++ src/scripts/snippets_CartItem.js
@@ -6,6 +6,20 @@ class CartItem extends LitElement {
   connectedCallback() {
     super.connectedCallback();
     // Listeners
+    const removeButton = this.querySelector('a');
+    removeButton.addEventListener('click', e => this.removeItem(e));
+  }
+
+  async removeItem(e) {
+    e.preventDefault();
+    const itemId = e.target.href.split('=')[1];
+    const fetchResponse = await fetch(
+      `/cart/change?id=${ itemId }&quantity=0`, 
+      {
+        method: 'POST',
+      },
+    );
+    console.log(fetchResponse);
   }
 }
 
