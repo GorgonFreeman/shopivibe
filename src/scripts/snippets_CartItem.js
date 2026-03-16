6d35777e2dc3af46bd2a35c7644823e9666a75c2
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-03-16 14:40:06 +1100

    mark for removal, then delete element pattern

--- src/scripts/snippets_CartItem.js
+++ src/scripts/snippets_CartItem.js
@@ -8,19 +8,26 @@ class CartItem extends LitElement {
     super.connectedCallback();
     // Listeners
     const removeButton = this.querySelector('a');
-    removeButton.addEventListener('click', e => this.removeItem(e));
+    removeButton.addEventListener('click', e => this.removeItemHandler(e));
   }
 
-  async removeItem(e) {
+  async removeItemHandler(e) {
     e.preventDefault();
     const itemId = e.target.href.split('=')[1];
-    const removeResponse = await customAxios(
+    const itemEl = e.target.closest('cart-item');
+    itemEl.classList.add('opacity-10');
+    const removeResponse = await this.removeItem(itemId);
+    console.log(removeResponse);
+    itemEl.remove();
+  }
+
+  async removeItem(itemId) {
+    return customAxios(
       `/cart/change.js?id=${ itemId }&quantity=0`,
       {
         method: 'post',
       },
     );
-    console.log(removeResponse);
   }
 }
 
