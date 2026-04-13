c68bdd34bbbb03fc885c974e62a37090a3b6de7b
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-04-13 16:23:49 +1000

    stub load more button

--- src/scripts/CollectionProducts.ts
+++ src/scripts/CollectionProducts.ts
@@ -1,7 +1,18 @@
-import { LitElement, html } from 'lit';
+import { LitElement } from 'lit';
+import { query } from 'lit/decorators.js';
 
 class CollectionProducts extends LitElement {
   createRenderRoot() { return this; }
+
+  @query('[data-ref="loadMoreButton"]') loadMoreButton;
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    this.loadMoreButton?.addEventListener('click', async () => {
+      console.log('load more');
+    });
+  }
 }
 
 customElements.define('collection-products', CollectionProducts);
