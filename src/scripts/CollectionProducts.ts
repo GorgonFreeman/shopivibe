13af4421ab899031186bafa4a41bbbe3a1b8ec06
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-04-13 16:33:11 +1000

    get nolayout data

--- src/scripts/CollectionProducts.ts
+++ src/scripts/CollectionProducts.ts
@@ -1,8 +1,12 @@
 import { LitElement } from 'lit';
 import { query } from 'lit/decorators.js';
+import { customFetch } from './utils';
 
 class CollectionProducts extends LitElement {
   createRenderRoot() { return this; }
+ 
+  // TODO: Get page from url params and update it ongoing
+  page = 1;
 
   @query('[data-ref="loadMoreButton"]') loadMoreButton;
 
@@ -11,6 +15,20 @@ class CollectionProducts extends LitElement {
 
     this.loadMoreButton?.addEventListener('click', async () => {
       console.log('load more');
+
+      const nextPageResponse = await customFetch(window.location.href, {
+        method: 'get',
+        params: {
+          page: this.page + 1,
+          view: 'nolayout.products_json',
+        },
+      });
+
+      console.log({ nextPageResponse });
+
+      const nextPageProducts = nextPageResponse?.result;
+
+      
     });
   }
 }
