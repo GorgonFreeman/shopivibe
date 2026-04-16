dbcd128cb5901bca6064a071c3bfd0e6731ebf1f
John <29110105+GorgonFreeman@users.noreply.github.com>
2026-04-16 14:13:38 +1000

    compose attach product tile click handler stub

--- src/scripts/CollectionProducts.ts
+++ src/scripts/CollectionProducts.ts
@@ -1,7 +1,26 @@
 import { LitElement } from 'lit';
+import './snippets_CustomModal';
 
 class CollectionProducts extends LitElement {
   createRenderRoot() { return this; }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.addEventListener('click', this.handleTileClick);
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this.removeEventListener('click', this.handleTileClick);
+  }
+
+  handleTileClick = (event: MouseEvent) => {
+    const target = event.target as Element;
+
+    console.log('handleTileClick', target);
+    event.preventDefault();
+    return;
+  };
 }
 
 customElements.get('collection-products') || customElements.define('collection-products', CollectionProducts);
