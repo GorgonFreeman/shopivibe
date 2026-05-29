8adaf26a7de7fa61ea009c0d6a1aafd7ed6c9876
Zwe <zwe@whitefoxboutique.com>
2026-05-29 13:00:06 +1000

    add typescript snippet for product price

---
+++ src/scripts/snippets_ProductPrice.ts
@@ -0,0 +1,35 @@
+import { html, nothing } from 'lit';
+import { LitElement } from 'lit';
+import { TemplateResult } from 'lit';
+import { property, state } from 'lit/decorators.js';
+import { VariantJSON } from './types/types';
+
+class ProductPrice extends LitElement {
+  createRenderRoot() { return this; }
+
+  @property({ type: Object, attribute: 'data-variant' })
+  variant?: VariantJSON;
+
+  async connectedCallback() {
+    super.connectedCallback();
+  }
+
+  render(): TemplateResult | typeof nothing {
+
+    const currency = window.Shopify.currency.active;
+    
+    if (!this.variant) {
+      return nothing;
+    }
+
+    return html`
+      ${ this.variant.compare_at_price && this.variant.compare_at_price > this.variant.price
+        ? html`
+          <s>${ this.variant.compare_at_price }</s>`
+        : nothing
+      }
+      <span>${ this.variant.price } ${ currency }</span>`
+    };
+}
+
+customElements.get('product-price') || customElements.define('product-price', ProductPrice);
